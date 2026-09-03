689f7388979f803f66f4cecc66e4825370f27275
manoj <sm6361098@gmail.com>
2026-09-03 22:34:52 +0545

    font

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export default function Home(){
   return(
     <div>
@@ -9,15 +10,15 @@ export default function Home(){
         <h6 className="flex justify-end mr-5">Date: 03-09-2026</h6>
         {/* News of the day */}
 
-        <h1 className="text-xl text-blue-400">Healthy Brain Scan /VS/ Suicidal Brain Scan</h1>
+        <h1 className="text-xl text-blue-400 mb-1 pl-2 font-serif">Healthy Brain Scan /VS/ Suicidal Brain Scan</h1>
 
-        <p className="text-sm bg-gray-900 border rounded-xl ml-1 mr-2 p-2 mb-2">September is Suicide Prevention Month.  After being a Psychiatrist for over 40 years, it's clear to me that suicidal thoughts and brains work differently. 
-           Amen Clinics has scanned hundreds of patients who have attempted suicide and many more who have thought about taking their own life. The SPECT scans of these patients revealed abnormalities in the brain, especially in these areas: 
+        <p className="text-sm bg-gray-400 text-black border rounded-xl ml-1 mr-2 p-2 mb-2 font-sans">September is Suicide Prevention Month.  After being a Psychiatrist for over 40 years, it's clear to me that suicidal thoughts and brains work differently. 
+          Amen Clinics has scanned hundreds of patients who have attempted suicide and many more who have thought about taking their own life. The SPECT scans of these patients revealed abnormalities in the brain, especially in these areas: 
           * Temporal Lobes: Brain SPECT imaging shows that people with suicidal thoughts and suicide attempts often have abnormalities in the left temporal lobe. In an Amen Clinics study, we saw left temporal lobe problems in 62 percent of our patients who had serious suicidal thoughts or behaviors. 
           * Traumatic Brain Injury (TBI): After reviewing nearly 300,000 functional brain scans related to behavior it is clear that head injuries are a leading cause of mental health issues, such as depression, anxiety disorders, panic disorder, PTSD, ADHD/ADD, substance abuse, psychosis, borderline and antisocial personality disorders, dementia, and aggression. Concussions and TBIs also increase the risk of suicide. When the brain has functional damage due to a head injury, it’s critical to heal the brain in order to overcome any mental health problems. At Amen Clinics, 40 percent of our patients have had a significant brain injury.
           * Prefrontal Cortex: A large-scale study by researchers at Portland State University found that about 22 percent of people who died by suicide were legally intoxicated at the time of their death. Brain scans show that alcohol lowers function in the prefrontal cortex, a brain region involved in impulse control, judgment, and decision-making.
           * Anterior Cingulate Gyrus: People with overactivity in this area of the brain tend to get stuck on negative thoughts, which is one of the traits we see in people with suicidal thoughts and behavior
-          </p>
+        </p>
         <img className="ml-4 pb-2" width={120} height={150} src="../down.jpg" alt="news" />
       </div>
 
