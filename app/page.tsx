dfc127d8296f89dbd2003adb7828cb9f65f7c811
manoj <sm6361098@gmail.com>
2026-09-04 08:46:12 +0545

    components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import News from "../components/News";
 export default function Home(){
   return(
     <div>
@@ -6,26 +7,29 @@ export default function Home(){
      
       <hr className="text-blue-300 mt-1 mb-1 " />
 
-      <div className="bg-gray-700">
-        <h6 className="flex justify-end mr-5">Date: 03-09-2026</h6>
         {/* News of the day */}
-
-        <h1 className="text-xl text-blue-400 mb-1 pl-2 font-serif">Healthy Brain Scan /VS/ Suicidal Brain Scan</h1>
-
-        <p className="text-sm bg-gray-400 text-black border rounded-xl ml-1 mr-2 p-2 mb-2 font-sans">September is Suicide Prevention Month.  After being a Psychiatrist for over 40 years, it's clear to me that suicidal thoughts and brains work differently. 
-          Amen Clinics has scanned hundreds of patients who have attempted suicide and many more who have thought about taking their own life. The SPECT scans of these patients revealed abnormalities in the brain, especially in these areas: 
-          * Temporal Lobes: Brain SPECT imaging shows that people with suicidal thoughts and suicide attempts often have abnormalities in the left temporal lobe. In an Amen Clinics study, we saw left temporal lobe problems in 62 percent of our patients who had serious suicidal thoughts or behaviors. 
-          * Traumatic Brain Injury (TBI): After reviewing nearly 300,000 functional brain scans related to behavior it is clear that head injuries are a leading cause of mental health issues, such as depression, anxiety disorders, panic disorder, PTSD, ADHD/ADD, substance abuse, psychosis, borderline and antisocial personality disorders, dementia, and aggression. Concussions and TBIs also increase the risk of suicide. When the brain has functional damage due to a head injury, it’s critical to heal the brain in order to overcome any mental health problems. At Amen Clinics, 40 percent of our patients have had a significant brain injury.
-          * Prefrontal Cortex: A large-scale study by researchers at Portland State University found that about 22 percent of people who died by suicide were legally intoxicated at the time of their death. Brain scans show that alcohol lowers function in the prefrontal cortex, a brain region involved in impulse control, judgment, and decision-making.
-          * Anterior Cingulate Gyrus: People with overactivity in this area of the brain tend to get stuck on negative thoughts, which is one of the traits we see in people with suicidal thoughts and behavior
-        </p>
-        <img className="ml-4 pb-2" width={200} height={100} src="../down.jpg" alt="news" />
-      </div>
+      {/* <div className="bg-gray-700"> */}
+        {/* Components */}
+        <News 
+          date="03-09-2026"
+          heading="Healthy Brain Scan /VS/ Suicidal Brain Scan"
+          newspart= {<> September is Suicide Prevention Month.  After being a Psychiatrist for over 40 years, it's clear to me that suicidal thoughts and brains work differently. 
+            Amen Clinics has scanned hundreds of patients who have attempted suicide and many more who have thought about taking their own life. The SPECT scans of these patients revealed abnormalities in the brain, especially in these areas: 
+            * Temporal Lobes: Brain SPECT imaging shows that people with suicidal thoughts and suicide attempts often have abnormalities in the left temporal lobe. In an Amen Clinics study, we saw left temporal lobe problems in 62 percent of our patients who had serious suicidal thoughts or behaviors. 
+            * Traumatic Brain Injury (TBI): After reviewing nearly 300,000 functional brain scans related to behavior it is clear that head injuries are a leading cause of mental health issues, such as depression, anxiety disorders, panic disorder, PTSD, ADHD/ADD, substance abuse, psychosis, borderline and antisocial personality disorders, dementia, and aggression. Concussions and TBIs also increase the risk of suicide. When the brain has functional damage due to a head injury, it’s critical to heal the brain in order to overcome any mental health problems. At Amen Clinics, 40 percent of our patients have had a significant brain injury.
+            * Prefrontal Cortex: A large-scale study by researchers at Portland State University found that about 22 percent of people who died by suicide were legally intoxicated at the time of their death. Brain scans show that alcohol lowers function in the prefrontal cortex, a brain region involved in impulse control, judgment, and decision-making.
+            * Anterior Cingulate Gyrus: People with overactivity in this area of the brain tend to get stuck on negative thoughts, which is one of the traits we see in people with suicidal thoughts and behavior
+            </>}
+          src="/down.jpg"
+          alt="windows image"
+          width={200}
+          height={200}
+          className=""
+          
+          />
 
       <hr className="text-blue-300 mt-1 mb-1 " />
-
-      <div className="mb-5"></div>
-
+    
     </div>
   )
 }
---
+++ components/News.tsx
@@ -0,0 +1,44 @@
+"use client";
+import Image from "next/image";
+
+type MyProps = {
+    heading: string;
+    date: string;
+    newspart:React.ReactNode;
+
+    // title: string Image for
+    src: string;
+    alt: string;
+    width? : number;
+    height?: number;
+    className?: string;
+};
+
+export default function News({heading,date,newspart, src, alt, width, height,className}:MyProps){
+    return(
+        
+      <div className="bg-gray-700">
+        <h6 className="flex justify-end mr-5">Date: {date}</h6> {/*Date of news */}
+
+        <h1 className="text-xl text-blue-400 mb-1 pl-2 font-serif">
+            {heading}
+          
+        </h1>  {/*This is Heading of news */}
+
+        <p className="text-sm bg-gray-400 text-black border rounded-xl ml-1 mr-2 p-2 mb-2 font-sans"> 
+            {newspart}
+        </p>
+
+        <Image 
+            src = {src}
+            alt = {alt}
+            width = {width}
+            height = {height}
+            className = {className}
+
+        />
+        <hr className="text-blue-300 mt-1 " />
+      </div>
+
+    )
+}
